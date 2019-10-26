b0aaf12ad61c86fba4564d9bca5b387faaa5d8c5
Bartosz Ujazdowski <bartosz.ujazdowski@comarch.com>
2019-10-26 15:46:52 +0200

    NN

--- src/SmpdApp.scala
+++ src/SmpdApp.scala
@@ -8,31 +8,12 @@ object SmpdApp extends App {
   val rawData = loadDataFromFile("data.txt")
   val data = PartitionedData(rawData, 0.80)
 
-    println("NearestNeighborAlgorithm")
-    new NearestNeighborAlgorithm(data).algorithm()
-    println("SeveralNearestNeighborAlgorithm")
-    new SeveralNearestNeighborAlgorithm(data, 100).algorithm()
+  println("NearestNeighborAlgorithm")
+  new NearestNeighborAlgorithm(data).algorithm()
 
+  println("SeveralNearestNeighborAlgorithm")
+  new SeveralNearestNeighborAlgorithm(data = data, countOfNeighborToMatch = 3).algorithm()
 
-  def testNN(): Unit = {
-    val testNNData = PartitionedData(
-      trainingData = Iterable(
-        ObjectClass("A", Map("C1" -> 1, "C2" -> 2, "C3" -> 3)),
-        ObjectClass("A", Map("C1" -> 2, "C2" -> 5, "C3" -> 3)),
-        ObjectClass("A", Map("C1" -> 2, "C2" -> 5, "C3" -> 4)),
-        ObjectClass("A", Map("C1" -> 4, "C2" -> 7, "C3" -> 5)),
-        ObjectClass("B", Map("C1" -> 1, "C2" -> 7, "C3" -> 1)),
-        ObjectClass("B", Map("C1" -> 3, "C2" -> 9, "C3" -> 1)),
-        ObjectClass("B", Map("C1" -> 3, "C2" -> 8, "C3" -> 2)),
-        ObjectClass("B", Map("C1" -> 5, "C2" -> 6, "C3" -> 1))
-      ),
-      testingData = Iterable(
-        ObjectClass("B", Map("C1" -> 1, "C2" -> 7, "C3" -> 3))
-      )
-    )
-
-    new NearestNeighborAlgorithm(testNNData).algorithm()
-  }
 
   private def loadDataFromFile(filename: String): Map[String, Vector[ObjectClass]] = {
     val source = fromFile(s"resources/$filename")
@@ -52,5 +33,25 @@ object SmpdApp extends App {
 
     source.close()
     result
+  }
+
+  private def testNN(): Unit = {
+    val testNNData = PartitionedData(
+      trainingData = Iterable(
+        ObjectClass("A", Map("C1" -> 1, "C2" -> 2, "C3" -> 3)),
+        ObjectClass("A", Map("C1" -> 2, "C2" -> 5, "C3" -> 3)),
+        ObjectClass("A", Map("C1" -> 2, "C2" -> 5, "C3" -> 4)),
+        ObjectClass("A", Map("C1" -> 4, "C2" -> 7, "C3" -> 5)),
+        ObjectClass("B", Map("C1" -> 1, "C2" -> 7, "C3" -> 1)),
+        ObjectClass("B", Map("C1" -> 3, "C2" -> 9, "C3" -> 1)),
+        ObjectClass("B", Map("C1" -> 3, "C2" -> 8, "C3" -> 2)),
+        ObjectClass("B", Map("C1" -> 5, "C2" -> 6, "C3" -> 1))
+      ),
+      testingData = Iterable(
+        ObjectClass("B", Map("C1" -> 1, "C2" -> 7, "C3" -> 3))
+      )
+    )
+
+    new NearestNeighborAlgorithm(testNNData).algorithm()
   }
 }
--- src/service/SeveralNearestNeighborAlgorithm.scala
+++ src/service/SeveralNearestNeighborAlgorithm.scala
@@ -2,12 +2,11 @@ package service
 
 import domain.{ObjectClass, PartitionedData}
 
-class SeveralNearestNeighborAlgorithm(dataSet: PartitionedData,
-                                      countOfNeighborToMatch: Int)
-  extends AlgorithmTemplate(dataSet) {
+class SeveralNearestNeighborAlgorithm(data: PartitionedData,
+                                      countOfNeighborToMatch: Int) extends AlgorithmTemplate(data) {
 
   override def classifyObject(objectClass: ObjectClass): ObjectClass = {
-    dataSet.trainingData.map(testingObject => (testingObject, distance(testingObject, objectClass)))
+    data.trainingData.map(testingObject => (testingObject, distance(testingObject, objectClass)))
       .toSeq
       .groupBy(_._1)
       .map(grouped => (grouped._1, grouped._2.map(touple => touple._2).sortBy(value => value).take(countOfNeighborToMatch).sum))
